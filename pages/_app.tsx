086a5b90b6a0bd717d916959e997a7d0ebcc556c
Ben Chomsang <43471788+1994benc@users.noreply.github.com>
2021-10-25 19:53:30 +0100

    Initial clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
-import 'tailwindcss/tailwind.css'
+import Head from "next/head";
+import React from "react";
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Choosable</title>
+        {/* TODO: <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
